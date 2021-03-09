3a0c145238649e508c98e00afc135d2088c9752a
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-03-09 10:40:33 +0100

    View stuff 2

--- src/client/core/ViewHandler.java
+++ src/client/core/ViewHandler.java
@@ -1,5 +1,7 @@
 package client.core;
 
+import client.view.chatroom.ChatRoomController;
+import client.view.usernamewindow.UserNameController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
--- src/client/view/chatroom/ChatRoomController.java
+++ src/client/view/chatroom/ChatRoomController.java
@@ -3,16 +3,26 @@ package client.view.chatroom;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ChatRoomController
 {
   @FXML public TextArea chatWindow;
+  @FXML public TextField messageField;
 
   @FXML public void onButtonClose(ActionEvent actionEvent)
   {
   }
 
   @FXML public void onButtonOpenClient(ActionEvent actionEvent)
+  {
+  }
+
+  @FXML public void onButtonSend(ActionEvent actionEvent)
+  {
+  }
+
+  @FXML public void onButtonConnections(ActionEvent actionEvent)
   {
   }
 }
