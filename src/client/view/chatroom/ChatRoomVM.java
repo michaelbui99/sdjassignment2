49b379a8106208abd461f5e2bd4a066943f0b83f
Michael Bui <michaelbui99@hotmail.com>
2021-03-11 08:42:08 +0100

    Added method

--- src/client/view/chatroom/ChatRoomVM.java
+++ src/client/view/chatroom/ChatRoomVM.java
@@ -1,15 +1,22 @@
 package client.view.chatroom;
 
 import client.model.ChatModel;
+import shared.Message;
+import shared.PropertyChangeSubject;
+
+import java.util.ArrayList;
 
 public class ChatRoomVM
 {
   private ChatModel model;
+  private ArrayList<Message> messages;
 
   public ChatRoomVM(ChatModel model)
   {
     this.model = model;
-
+    ((PropertyChangeSubject) model).addPropertyChangeListener((evt)->{
+    messages.add((Message) evt.getNewValue());
+    });
   }
 
   public void sendMessage(String msg)
